4ee7592f7cfc6c8f19eba40e467b222c2603a4f4
riahichaima <chaymariahi14@gmail.com>
2025-07-26 14:15:31 +0100

    correction ex 1

--- src/app/home/home.ts
+++ src/app/home/home.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-
+import { Article } from '../models';
+import { Provider } from '../models';
 @Component({
   selector: 'app-home',
   standalone: false,
@@ -7,5 +8,10 @@ import { Component } from '@angular/core';
   styleUrl: './home.css'
 })
 export class Home {
+  providers:Provider[] = [
+    {id: 1, name: 'samsung', address: 'kore', email:'samsung@gmail.com' },
+    {id: 2, name: 'apple', address: 'usa', email:'apple@gmail.com' },
+    {id: 3, name: 'xiaomi', address: 'china', email:'xiaomi@gmail.com' }
+  ];
 
 }
